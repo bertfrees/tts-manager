3dce0e7ab399702a52fd54be39bc07d65dd33516
Bert Frees <bertfrees@gmail.com>
2012-01-17 21:36:41 +0100

    Added license information

--- src/org/bertfrees/tts/mac/MacOSVoice.java
+++ src/org/bertfrees/tts/mac/MacOSVoice.java
@@ -1,15 +1,36 @@
+/**
+ *  Copyright (c) 2012 Bert Frees <bertfrees@gmail.com>
+ * 
+ *  This file is part of tts-manager <https://github.com/bertfrees/tts-manager>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.bertfrees.tts.mac;
 
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Collection;
-
+import org.bertfrees.tts.Voice;
 import org.rococoa.Rococoa;
 import org.rococoa.cocoa.foundation.NSArray;
 import org.rococoa.cocoa.foundation.NSAutoreleasePool;
 
-import org.bertfrees.tts.Voice;
-
+/**
+ *
+ * @author Bert Frees
+ */
 public class MacOSVoice extends Voice {
     
     private final static boolean IS_MAC = System.getProperty("os.name").toLowerCase().contains("mac os");
